9e2279de75532f6b7237101ca45c8e395f455fa3
yutaroadachi <48372923+yutaroadachi@users.noreply.github.com>
2023-04-25 15:41:40 +0900

    chore: update dependencies

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,6 @@
-import Vue from "@vitejs/plugin-vue";
 import { defineVitestConfig } from "nuxt-vitest/config";
 
 export default defineVitestConfig({
-  plugins: [Vue()],
   test: {
     environment: "jsdom",
     include: ["src/**/*.test.ts"],
