131daa021a60573038de1220f67ac48031d720af
yutaroadachi <48372923+yutaroadachi@users.noreply.github.com>
2023-04-25 15:51:55 +0900

    chore: Nuxt DevToolsを追加

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   css: ["~/assets/css/main.css"],
-  modules: ["nuxt-vitest"],
+  modules: ["@nuxt/devtools", "nuxt-vitest"],
   postcss: {
     plugins: {
       tailwindcss: {},
